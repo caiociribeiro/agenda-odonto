c05d70fb5a285e78b070f9b53a86f21e850e97a2
Marcelo Figueiredo <marcelofigdev@gmail.com>
2024-11-06 15:13:50 -0300

    Comitando pra dar pull

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     signingConfigs {
         getByName("debug") {
-            storeFile = file("C:\\Users\\caio\\.android\\debug.keystore")
+            storeFile = file("C:\\Users\\marce\\.android\\debug.keystore")
         }
     }
     namespace = "com.example.agendaodonto"
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.material.slide.date.picker)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
